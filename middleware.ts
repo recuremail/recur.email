4205a2b36939cb656797f8f08e2fcf62968877e7
Anton Podviaznikov <podviaznikov@gmail.com>
2024-10-18 12:11:57 +0200

    redirect recur e,ail

--- middleware.ts
+++ middleware.ts
@@ -2,79 +2,82 @@ import { is } from "date-fns/locale"
 import { getToken } from "next-auth/jwt"
 import { withAuth } from "next-auth/middleware"
 import { NextResponse, NextFetchEvent, NextRequest } from "next/server"
-import { detectBot, parse } from "@/lib/middleware";
+import { detectBot, parse } from "@/lib/middleware"
 
-import { recordView } from "@/lib/tinybird";
+import { recordView } from "@/lib/tinybird"
 
 export default withAuth(
   async function middleware(req: NextRequest, ev: NextFetchEvent) {
     const { pathname } = req.nextUrl
-    
-    if(pathname.endsWith('.php') || 
-      pathname.endsWith('.html') || 
-      pathname.endsWith('.env') || 
-      pathname.endsWith('.env.prod') || 
-      pathname.endsWith('.env.bak') ||
-      pathname.endsWith('.env.dev') || 
-      pathname.endsWith('/.svn/entries') || 
-      pathname.endsWith('/.aws/credentials') || 
-      pathname.endsWith('/phpinfo') || 
-      pathname.endsWith('app-manifest.json') || 
-      pathname.indexOf('wp-json') > -1 ||
-      pathname.indexOf('DESKTOP_API_URL') > - 1 ||
-      pathname.indexOf('404') > - 1 ||
-      pathname.indexOf('_ignition/health-check') > - 1 ||
-      pathname.endsWith('.CityList') ||
-      pathname.endsWith('ads.txt') ||
-      pathname.endsWith('app-ads.txt') ||
-      pathname.endsWith('/_profiler/phpinfo') ||
-      pathname.endsWith('/admin/allowurl.txt')) {
-        return NextResponse.redirect('/404')
+
+    if (
+      pathname.endsWith(".php") ||
+      pathname.endsWith(".html") ||
+      pathname.endsWith(".env") ||
+      pathname.endsWith(".env.prod") ||
+      pathname.endsWith(".env.bak") ||
+      pathname.endsWith(".env.dev") ||
+      pathname.endsWith("/.svn/entries") ||
+      pathname.endsWith("/.aws/credentials") ||
+      pathname.endsWith("/phpinfo") ||
+      pathname.endsWith("app-manifest.json") ||
+      pathname.indexOf("wp-json") > -1 ||
+      pathname.indexOf("DESKTOP_API_URL") > -1 ||
+      pathname.indexOf("404") > -1 ||
+      pathname.indexOf("_ignition/health-check") > -1 ||
+      pathname.endsWith(".CityList") ||
+      pathname.endsWith("ads.txt") ||
+      pathname.endsWith("app-ads.txt") ||
+      pathname.endsWith("/_profiler/phpinfo") ||
+      pathname.endsWith("/admin/allowurl.txt")
+    ) {
+      return NextResponse.redirect("/404")
     }
 
     const hostname = req.headers.get("host") || ""
-    console.log('hostname', hostname)
+    console.log("hostname", hostname)
 
-    if(hostname == 'newsletter.new') {
-      return NextResponse.redirect(new URL("https://recur.email/new?from=newsletter.new"))
+    if (hostname == "newsletter.new") {
+      return NextResponse.redirect(
+        new URL("https://recuremail.com/new?from=newsletter.new")
+      )
+    }
+    if (hostname == "recur.email") {
+      return NextResponse.redirect(new URL("https://recuremail.com"))
     }
     const token = await getToken({ req })
-    const url = req.nextUrl.clone();
-    
-    const isAuth = !!token
+    const url = req.nextUrl.clone()
 
+    const isAuth = !!token
 
     const isAuthPage =
       req.nextUrl.pathname.startsWith("/login") ||
       req.nextUrl.pathname.startsWith("/register")
 
-    // console.log("isAuth", url.href, isAuth, isAuthPage)  
+    // console.log("isAuth", url.href, isAuth, isAuthPage)
     if (isAuthPage) {
       if (isAuth) {
-        console.log('redirecting to dashboard??', url.searchParams)
+        console.log("redirecting to dashboard??", url.searchParams)
         return NextResponse.redirect(new URL("/dashboard", req.url))
       }
 
       return null
     }
 
-    const { domain, fullKey: key, path } = parse(req);
-    const isBot = detectBot(req);
-    
-    let from = req.nextUrl.pathname;
+    const { domain, fullKey: key, path } = parse(req)
+    const isBot = detectBot(req)
 
-
+    let from = req.nextUrl.pathname
 
     if (req.nextUrl.search) {
-      from += req.nextUrl.search;
+      from += req.nextUrl.search
     }
     // if custom domain we need to redirect here?
     // we can use edge config to do this later.
 
-    
     const referer = req.headers.get("referer")
     // console.log('domain, key', domain, key, from, isAuth, referer, req.nextUrl)
-    
+
     // if (!isBot && key && !req.headers.get("kiwi-no-track") && (!referer || referer?.indexOf('https://recur.email/dashboard') == -1)) {
     //   // we only track track recur.email/anton/note-id
     //   const pathElements = path.split('/').filter((t) => t && t.length > 0)
@@ -85,24 +88,24 @@ export default withAuth(
     //     }
     //   }
     // }
-    console.log('can we get here?', key, isAuth, req.url, req.nextUrl)
-    if(!isAuth && key == 'dashboard') {
+    console.log("can we get here?", key, isAuth, req.url, req.nextUrl)
+    if (!isAuth && key == "dashboard") {
       return NextResponse.redirect(new URL("/login", req.url))
     }
     const subdomain =
-    process.env.NODE_ENV === "production" && process.env.VERCEL === "1"
-      ? hostname
-          .replace(`.recur.email`, "")
-          .replace(`.recur-email.vercel.app`, "")
-      : hostname.replace(`.localhost:3000`, "");
+      process.env.NODE_ENV === "production" && process.env.VERCEL === "1"
+        ? hostname
+            .replace(`.recur.email`, "")
+            .replace(`.recur-email.vercel.app`, "")
+        : hostname.replace(`.localhost:3000`, "")
 
     // if custom domain we need to redirect here?
-    if(hostname.indexOf(".recur.email") > -1) {
+    if (hostname.indexOf(".recur.email") > -1) {
       // redirected to the newsletter
-      console.log('rewrite to', `/newsletters/${subdomain}${path}`)
+      console.log("rewrite to", `/newsletters/${subdomain}${path}`)
       return NextResponse.rewrite(
         new URL(`/newsletters/${subdomain}${path}`, req.url)
-      );
+      )
     }
     // if (!isAuth) {
     //   console.log('from what is this', from, domain, key)
@@ -110,7 +113,7 @@ export default withAuth(
     //     new URL(`/login?from=${encodeURIComponent(from)}`, req.url)
     //   );
     // }
-    return NextResponse.next();
+    return NextResponse.next()
   },
   {
     callbacks: {
@@ -118,7 +121,7 @@ export default withAuth(
         // This is a work-around for handling redirect on auth pages.
         // We return true here so that the middleware function above
         // is always called.
-        console.log('authorized callback.')
+        console.log("authorized callback.")
         return true
       },
     },
@@ -137,8 +140,6 @@ export const config = {
      * 5. /_vercel (Vercel internals)
      * 6. /favicon.ico, /sitemap.xml, /site.webmanifest (static files)
      */
-    "/((?!api/|_next/|_proxy/|_auth/|_static|_vercel|favicon.ico|favicon.png|favicon.svg|og.png|sitemap.xml|site.webmanifest).*)"
+    "/((?!api/|_next/|_proxy/|_auth/|_static|_vercel|favicon.ico|favicon.png|favicon.svg|og.png|sitemap.xml|site.webmanifest).*)",
   ],
-  
-  
 }
